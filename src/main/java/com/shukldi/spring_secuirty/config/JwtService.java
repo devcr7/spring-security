49a01c918f91b6535a6feb8ab0b0765bdf9e79b8
Divyanshu Shukla <divyanshushuklazz10@gmail.com>
2025-03-23 01:10:52 +0530

    Added authenticator filter service and user repo

---
+++ src/main/java/com/shukldi/spring_secuirty/config/JwtService.java
@@ -0,0 +1,11 @@
+package com.shukldi.spring_secuirty.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+
+    public String extractUserName(String token) {
+        return null;
+    }
+}
---
+++ src/main/java/com/shukldi/spring_secuirty/user/Role.java
@@ -0,0 +1,6 @@
+package com.shukldi.spring_secuirty.user;
+
+public enum Role {
+    USER,
+    ADMIN;
+}
