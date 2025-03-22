92b491fb7b7e22ac51ca02ca7e1d8a96e1e4dd81
Divyanshu Shukla <divyanshushuklazz10@gmail.com>
2025-03-23 01:29:26 +0530

    Added singing/secret key creation

--- src/main/java/com/shukldi/spring_secuirty/config/JwtService.java
+++ src/main/java/com/shukldi/spring_secuirty/config/JwtService.java
@@ -1,11 +1,34 @@
 package com.shukldi.spring_secuirty.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
 
+    @Value("${security.jwt.secret-key}")
+    private static String SECRET_KEY;
+
     public String extractUserName(String token) {
         return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+       return Jwts.parser()
+               .setSigningKey(getSigningKey())
+               .build()
+               .parseClaimsJws(token)
+               .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
